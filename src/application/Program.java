f324e80beb73f3375752e7e3a4af7e7f579a2d20
unknown <jpbusinesss@hotmail.com>
2025-02-19 17:55:49 -0300

    criando a classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Fala puta");
+		
+		Position poss = new Position(3,5);
+		System.out.println(poss);
 
 	}
 
